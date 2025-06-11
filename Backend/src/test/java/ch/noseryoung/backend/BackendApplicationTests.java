426b84361507ec75db4c19df9be59add6e21cbc9
Nepomuk Crhonek <105591323+Nepomuk5665@users.noreply.github.com>
2025-06-11 09:13:44 +0200

    Created spring backend project

---
+++ Backend/src/test/java/ch/noseryoung/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ch.noseryoung.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
