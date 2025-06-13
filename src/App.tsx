9e1d15d5f44938799dd5630b45b97077f9b96bf4
sylia08 <sylia.scandinaro@edu.tbz.ch>
2025-06-13 11:09:04 +0200

    fix routes #v0.3.1

--- src/App.tsx
+++ src/App.tsx
@@ -1,20 +1,3 @@
-<<<<<<< HEAD
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import Homepage from './pages/Homepage'
-import MenuPage from './pages/MenuPage'
-import MenuItemPage from './pages/MenuItemPage'
-import LoginPage from './pages/LoginPage'
-import SignupPage from './pages/SignupPage'
-import ReservationPage from './pages/ReservationPage'
-import DashboardPage from './pages/DashboardPage'
-import { ContactPage } from '@mui/icons-material'
-import NotFoundPage from './pages/NotFoundPage'
-import PrivateRoute from './components/PrivateRoute'
-import MenuItemEditPage from './pages/MenuItemEditPage'
-import MenuItemCreatePage from './pages/MenuItemCreatePage'
-import TablesPage from './pages/TablesPage'
-import TablesCreatePage from './pages/TablesCreatePage'
-=======
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Homepage from "./components/pages/Homepage";
 import MenuPage from "./components/pages/MenuPage";
@@ -26,34 +9,37 @@ import DashboardPage from "./components/pages/DashboardPage";
 import { ContactPage } from "@mui/icons-material";
 import NotFoundPage from "./components/pages/NotFoundPage";
 import PrivateRoute from "./components/PrivateRoute";
->>>>>>> feat/homepage
+import MenuItemEditPage from "./components/pages/MenuItemEditPage";
+import MenuItemCreatePage from "./components/pages/MenuItemCreatePage";
+import TablesPage from "./components/pages/TablesPage";
+import TablesCreatePage from "./components/pages/TablesCreatePage";
 
 function App() {
   return (
     <>
       <BrowserRouter>
         <Routes>
-<<<<<<< HEAD
-            <Route path="/" element={<Homepage />} />
-            <Route path="/login" element={<LoginPage />} />
-            <Route path="/signup" element={<SignupPage />} />
-            <Route path="/menu" element={<MenuPage />} />
-            <Route path="/menu/:id" element={<MenuItemPage />}/>
-            <Route path="/menu/:id/edit" element={<PrivateRoute><MenuItemEditPage /></PrivateRoute>}/>
-            <Route path="/menu/new" element={<PrivateRoute><MenuItemCreatePage /></PrivateRoute>}/>
-            <Route path="/reservation" element={<ReservationPage />}/>
-            <Route path="/dashboard" element={<PrivateRoute><DashboardPage /></PrivateRoute>}/>
-            <Route path="/contact" element={<ContactPage />} />
-            <Route path="/tables" element={<PrivateRoute><TablesPage /></PrivateRoute>} />
-            <Route path="/tables/new" element={<PrivateRoute><TablesCreatePage /></PrivateRoute>} />
-            <Route path="*" element={<NotFoundPage />} />
-          </Routes>
-=======
           <Route path="/" element={<Homepage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/signup" element={<SignupPage />} />
           <Route path="/menu" element={<MenuPage />} />
           <Route path="/menu/:id" element={<MenuItemPage />} />
+          <Route
+            path="/menu/:id/edit"
+            element={
+              <PrivateRoute>
+                <MenuItemEditPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/menu/new"
+            element={
+              <PrivateRoute>
+                <MenuItemCreatePage />
+              </PrivateRoute>
+            }
+          />
           <Route path="/reservation" element={<ReservationPage />} />
           <Route
             path="/dashboard"
@@ -64,9 +50,24 @@ function App() {
             }
           />
           <Route path="/contact" element={<ContactPage />} />
+          <Route
+            path="/tables"
+            element={
+              <PrivateRoute>
+                <TablesPage />
+              </PrivateRoute>
+            }
+          />
+          <Route
+            path="/tables/new"
+            element={
+              <PrivateRoute>
+                <TablesCreatePage />
+              </PrivateRoute>
+            }
+          />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
->>>>>>> feat/homepage
       </BrowserRouter>
     </>
   );
