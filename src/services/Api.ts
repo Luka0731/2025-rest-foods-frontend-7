b4ec3e29af59c10640c98cb1e561223cc5c9de59
Diego De Corso <diego.decorso@edu.tbz.ch>
2025-06-18 14:10:04 +0200

    change base url

--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "http://localhost:8080/api",
 });
 
 api.interceptors.request.use((config) => {
