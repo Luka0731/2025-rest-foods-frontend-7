6ad74aaf2d596c061a972d55a7e2d8b525a96ebb
sylia08 <sylia.scandinaro@edu.tbz.ch>
2025-06-24 13:12:40 +0200

    add login

--- src/components/pages/LoginPage.tsx
+++ src/components/pages/LoginPage.tsx
@@ -1,5 +1,48 @@
-const LoginPage = () => {
-  return <>Login Page</>;
+import React, { useState } from "react";
+import LoginForm from "../organismes/LoginForm";
+import { AuthService } from "../../services/AuthenticationService";
+import { useNavigate } from "react-router-dom";
+
+interface LoginFormValues {
+  email: string;
+  password: string;
+}
+
+const Login: React.FC = () => {
+  const navigate = useNavigate();
+  const [apiError, setApiError] = useState("");
+
+  const initialValues: LoginFormValues = {
+    email: "",
+    password: "",
+  };
+
+  const handleSubmit = async (values: LoginFormValues) => {
+    try {
+      const result = await AuthService.login(values.email, values.password);
+      localStorage.setItem("accessToken", result.accessToken);
+      navigate("/");
+    } catch (error: unknown) {
+      if (error instanceof Error) {
+        setApiError(error.message);
+      } else {
+        setApiError("Login failed");
+      }
+    }
+  };
+
+  return (
+    <>
+      <div className="Login">
+        <h1>Login</h1>
+        <LoginForm
+          initialValues={initialValues}
+          onSubmit={handleSubmit}
+          apiError={apiError}
+        />
+      </div>
+    </>
+  );
 };
 
-export default LoginPage;
+export default Login;
--- src/services/Api.ts
+++ src/services/Api.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 export const api = axios.create({
-  baseURL: "http://localhost:8080/api",
+  baseURL: "http://localhost:8081/api",
 });
 
 api.interceptors.request.use((config) => {
