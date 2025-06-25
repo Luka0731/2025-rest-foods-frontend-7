611d8265e2ad3b8d9426e6433a67afcd99454158
Luka Grujic <luka.grujic@edu.tbz.ch>
2025-06-25 08:21:41 +0200

    add pictures

--- src/components/atoms/MenuBarFilterTab.tsx
+++ src/components/atoms/MenuBarFilterTab.tsx
@@ -3,20 +3,10 @@ import "./MenuBarTab.css";
 
 const MenuBarFilterTab = () => {
 
-    let filterBar = null;
-    const handleClick = () => {
-        if (filterBar == null) {
-            filterBar = (<FilterBar/>);
-        } else {
-            filterBar = null;
-        }
-    };
 
     return (
-        <div className="menu-bar-tab" onClick={handleClick}>
-            <img className="icon" src="" alt="icon" /> {/* ToDo */}
-            <p className="title">Filter</p>
-            {filterBar}
+        <div className="menu-bar-tab">
+
         </div>
     );
 
